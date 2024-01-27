68b8035fa1e1681534f6a076701bddaa7c784b2b
sajith_nilanka <124520853+sajithnilanka@users.noreply.github.com>
2024-01-27 17:16:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+import os
 from rembg import remove
-import easygui
-from PIL import image
+from PIL import Image
 
-input = easygui.fileopenbox(title="select image file..")
-outputpath =easygui.fileopenbox(title="save file to...")
+#Define input and output folders
+input_folder = "input_images"
+output_folder = "output_images"
 
-input = image.open(inputpath)
-output =remove(input)
-output.save(outputpath)
+#loop through all the files in the input folder
+for filename in os.listdir(input_folder):
+    #check if the file is an image file
+    if filename.endswith('.jpg') or filename.endswith('.png'):
+        #Define input and output file paths
+        input_path = os.path.join(input_folder,filename)
+        output_path = os.path.join(output_folder,filename.split('.')[0]+'.png')
+
+        #load the input image
+        input_image = Image.open(input_path)
+
+        #Remove the background using rembg
+        output_image = remove(input_image)
+
+        #save the output image as a PNG file
+        output_image.save(output_path)
